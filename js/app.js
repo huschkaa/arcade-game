92a8ae965414ae4f702f5016eab0bde8b8772116
Andrew Huschka <huschkaa@gmail.com>
2017-11-28 10:46:46 -0600

    completed initial functions to start building out the game

--- js/app.js
+++ js/app.js
@@ -1,42 +1,46 @@
 // Enemies our player must avoid
-var allEnemies = []; /*Added to correct error in engine and will need to use variable later*/
-
-var Enemy = function() {
+var Enemy = function(x,y) {
     // The image/sprite for our enemies, this load images for the enemies.
     this.sprite = 'images/enemy-bug.png';
+    this.x;
+    this.y;
 };
+
+//Create enemy bugs that go across the screen
+var allEnemies = [new Enemy(100,250)];
 
 // Update the enemy's position, required method for game. Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter. which will ensure the game runs at the same speed for all computers.
+    moveEnemy = this.x*dt;
+    moveEnemy++;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
 };
 
 // Now write your own player class
-var player = function() {
+var Player = function(x,y) {
     // The image/sprite for our player, this load images for our player.
     this.sprite = 'images/char-boy.png';
+    this.x;
+    this.y;
 };
+
+var player = new Player(200,400); /* sets starting position of the player*/
 
 // This class requires an update(), render() and
-player.update = function() {
-    this.sprite = 'images/char-boy.png';
+Player.prototype.update = function() { }
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-player.render = function () {
-      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-
 //Function added to ensure code works properly need to determine what goes in the function
 
 // a handleInput() method.
-
+Player.prototype.handleInput = function(key) { }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
